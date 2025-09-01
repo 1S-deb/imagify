40bdebe466f88c0342285f587364a43dcf9d0ac9
Srinjana Deb <srinjanadeb@gmail.com>
2025-09-01 22:07:22 +0530

    Initial commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,11 +30,12 @@ const Navbar = () => {
             </div>
         </div>
         :
-        <div className='flex items-center gap-2 sm gap-5'>
-            { <p onClick={()=>navigate('/buy')}className='cursor-pointer '>Pricing</p> }
-            <button onClick={()=>setShowLogin(true)}className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-small rounded-full'>Login</button>
-        </div>
-        }
+        
+         <div className='flex items-center gap-2 sm:gap-5'>
+             {/* <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p> */}
+             <button onClick={() => setShowLogin(true)} className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-small rounded-full'>Login</button>
+           </div>
+         }
        
      </div>
     </div>
